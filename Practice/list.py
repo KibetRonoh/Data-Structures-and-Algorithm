e581506dc297f84064cd3863a22257e7522998eb
kibet hillary <kibethillaire@gmail.com>
2022-05-16 00:24:25 +0300

    update

--- Practice/list.py
+++ Practice/list.py
@@ -1,33 +1,24 @@
-"""
-Given an array of integers nums and an integer target, return indices of the two numbers 
-such that they add up to target.
+##########
+list = [1, 2, 3, 4, 4]
+list.append('kibet')
+list.insert(3, 'Ronoh')
+list.extend([2, 'hillary', '$ 2500'])
 
-You may assume that each input would have exactly one solution, and you may not use the 
-same element twice.
+print(list)
 
-You can return the answer in any order.
+# adding multiple elements using append method
+lost = [8, 9, 78, 6, 45]
 
- 
+for i in range(1, 9):
+    lost.append(i)
+print(lost)
 
-Example 1:
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+lost.pop()
+print(lost)
 
-Example 2:
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
+lost.pop(3)
+print(lost)
 
-Example 3:
-Input: nums = [3,3], target = 6
-Output: [0,1]
-"""
-########################################
-#SOLUTION
-########################################
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[j] == target - nums[i]:
-                    return [i, j]
+lost.remove(4)
+print(lost)
+

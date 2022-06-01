af1727877f8d1a0b171a6045e418aa456af1ce4e
kibet hillary <kibethillaire@gmail.com>
2022-06-02 00:40:18 +0300

    update

--- list.py
+++ list.py
@@ -77,7 +77,10 @@ print (functools.reduce(lambda a,b: a if (a > b) else b, [7, 12, 45, 100, 15]))
 
 
 # Python program to merge and sort two list
-
+def Merge(list1, list2):
+	final_list = list1 + list2
+	final_list.sort()
+	return(final_list)
 
 # Driver Code
 list1 = [25, 18, 9, 41, 26, 31]

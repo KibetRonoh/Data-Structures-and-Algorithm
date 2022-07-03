72e8c5db27e360ee82fc4a0eb69341f8ac9a11b8
kibet hillary <kibethillaire@gmail.com>
2022-07-03 21:46:22 +0300

    recommit

--- list-comprehension.py
+++ list-comprehension.py
@@ -10,9 +10,7 @@ print(even_squares)
 # for understanding, above generation is same as,
 odd_square1 = []
  
-for x in range(1, 11):
-    if x % 2 == 1:
-        odd_square1.append(x**2)
+
  
 print(odd_square1)
 
